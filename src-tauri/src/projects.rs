f9060aaa07b3ff74331431427b87fe58fd312305
tobinio <Tobias.frischmann1@gmail.com>
2024-09-11 20:15:54 +0200

    only show folders in project tab

--- src-tauri/src/projects.rs
+++ src-tauri/src/projects.rs
@@ -2,6 +2,7 @@ use crate::error;
 use crate::settings::Settings;
 use applications::App;
 use serde::{Deserialize, Serialize};
+use std::ffi::OsStr;
 use std::fs::read_dir;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
@@ -16,9 +17,10 @@ pub fn get_projects(settings: Settings) -> error::Result<Vec<Project>> {
     if settings.path.is_none() {
         return Ok(vec![]);
     }
-    
+
     let names = read_dir(settings.path.unwrap_or_default())?
         .filter_map(Result::ok)
+        .filter(|e| e.path().is_dir())
         .filter(|dir| dir.file_name().into_string().is_ok())
         .map(|dir| Project {
             path: dir.path(),
@@ -32,7 +34,14 @@ pub fn get_projects(settings: Settings) -> error::Result<Vec<Project>> {
 pub fn run_project(project: Project, app: App) {
     //TODO - https://github.com/HuakunShen/applications-rs/issues/5
     let exec = app.app_path_exe.unwrap().to_str().unwrap().to_owned();
-    let exec = exec.replace("%u", "").replace("%F", "").replace("%U", "").replace("%f", "").replace("\"", "").trim().to_string();
+    let exec = exec
+        .replace("%u", "")
+        .replace("%F", "")
+        .replace("%U", "")
+        .replace("%f", "")
+        .replace("\"", "")
+        .trim()
+        .to_string();
 
     Command::new(exec)
         .arg(project.path.to_str().unwrap())
