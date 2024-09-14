6d27b281cbecca4b5d37092275e6f69a454b7fc6
benni <benni.huff@gmail.com>
2024-09-14 21:21:16 +0200

    added dropdown menu and runtime save for used apps

--- app/components/Projects.vue
+++ app/components/Projects.vue
@@ -8,6 +8,14 @@ let projects = await useProjects(settings);
 async function onRunProject(project: Project, app: App) {
   await invoke("run_project", {project: project, app: app})
 }
+
+function addToUsed(project: Project, app: App) {
+  project.used.push(app.name)
+}
+
+function removeFromUsed(project, app) {
+  project.used = project.used.filter((usedApp) => usedApp.name !== app.name);
+}
 </script>
 
 <template>
@@ -15,13 +23,33 @@ async function onRunProject(project: Project, app: App) {
     <h4> {{ project.name }} </h4>
 
     <div class="flex gap-1">
-      <button @click="() => onRunProject(project, app)" :key="app.name" v-for="app of settings.apps" class="hover:scale-125 transition">
-        <SmallAppEntry :app="app"/>
-      </button>
+      <div v-for="app of settings.apps">
+        <button v-if="project.used.includes(app.name)" @click="() => addToUsed(project, app)" :key="app.name"
+                class="hover:scale-125 transition">
+          <SmallAppEntry :app="app"/>
+        </button>
+      </div>
+    </div>
+
+    <div>
+      <select @change="(event) => {
+        let app = JSON.parse(event.target.value);
+
+        addToUsed(project, app);
+        onRunProject(project, app);
+      }">
+        <option disabled selected>Select an app</option>
+        <option :disabled="project.used.includes(app.name)" v-for="app of settings.apps" :key="app.name"
+                :value="JSON.stringify(app)">
+          {{ app.name }}
+        </option>
+      </select>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+select {
+  color: black;
+}
 </style>
