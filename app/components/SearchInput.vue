7eeb7620192cb63f908434cc5ba3e5cbd062582c
tobinio <Tobias.frischmann1@gmail.com>
2024-09-15 10:54:16 +0200

    extract SearchInput

---
+++ app/components/SearchInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+let text = defineModel("text")
+
+let inputRef = useTemplateRef("input")
+
+onKeyStroke("f", event => {
+  if (!event.ctrlKey) return
+
+  inputRef.value.focus()
+})
+</script>
+
+<template>
+  <div class="input h-7 bg-zinc-700 m-2 rounded-md w-max flex items-center z-10">
+    <Icon class="mx-1.5" name="ph:magnifying-glass-bold" size="18"/>
+    <input ref="input" class="h-full bg-transparent !outline-none" type="text" v-model="text">
+  </div>
+</template>
+
+<style scoped>
+.input {
+  &:has(input:focus-visible) {
+    @apply bg-zinc-600
+  }
+}
+</style>
--- app/pages/apps.vue
+++ app/pages/apps.vue
@@ -3,30 +3,17 @@ import AppList from "~/components/apps/AppList.vue";
 
 let apps = await useApps()
 
-let searchText = ref("")
-
+let searchText = ref("");
 let filteredApps = computed(() => {
   return apps.value
       .sort((a, b) => a.name.localeCompare(b.name))
       .filter(app => app.name.toUpperCase().includes(searchText.value.toUpperCase()))
 })
-
-let inputRef = useTemplateRef("input")
-
-onKeyStroke("f", event => {
-  if (!event.ctrlKey) return
-
-  inputRef.value.focus()
-})
-
 </script>
 
 <template>
   <div class="bg-zinc-900 fixed w-full">
-    <div class="input h-7 bg-zinc-700 m-2 rounded-md w-max flex items-center z-10">
-      <Icon class="mx-1.5" name="ph:magnifying-glass-bold" size="18"/>
-      <input ref="input" class="h-full bg-transparent !outline-none" type="text" v-model="searchText">
-    </div>
+    <SearchInput v-model:text="searchText"/>
   </div>
 
   <div class="mt-9">
@@ -35,9 +22,4 @@ onKeyStroke("f", event => {
 </template>
 
 <style scoped>
-.input {
-  &:has(input:focus-visible) {
-    @apply bg-zinc-600
-  }
-}
 </style>
