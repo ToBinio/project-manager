23644ad374bbe0fdf979edb0d2ba08e510aaa9f8
tobinio <Tobias.frischmann1@gmail.com>
2024-09-15 10:42:49 +0200

    improve app search

--- app/pages/apps.vue
+++ app/pages/apps.vue
@@ -11,15 +11,33 @@ let filteredApps = computed(() => {
       .filter(app => app.name.toUpperCase().includes(searchText.value.toUpperCase()))
 })
 
+let inputRef = useTemplateRef("input")
+
+onKeyStroke("f", event => {
+  if (!event.ctrlKey) return
+
+  inputRef.value.focus()
+})
 
 </script>
 
 <template>
-  <input class="border-2 rounded-md mx-5 my-3" type="text" v-model="searchText">
+  <div class="bg-zinc-900 fixed w-full">
+    <div class="input h-7 bg-zinc-700 m-2 rounded-md w-max flex items-center z-10">
+      <Icon class="mx-1.5" name="ph:magnifying-glass-bold" size="18"/>
+      <input ref="input" class="h-full bg-transparent !outline-none" type="text" v-model="searchText">
+    </div>
+  </div>
 
-  <AppList :apps="filteredApps"/>
+  <div class="mt-9">
+    <AppList :apps="filteredApps"/>
+  </div>
 </template>
 
 <style scoped>
-
+.input {
+  &:has(input:focus-visible) {
+    @apply bg-zinc-600
+  }
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,5 +21,5 @@ export default defineNuxtConfig({
         },
     },
 
-    modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/icon'],
+    modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/icon', '@vueuse/nuxt'],
 })
